15d7a2d44d41bba9bfeaec61e195e7768b5e0a61
sparrow <alevar@gmail.com>
2023-10-07 23:36:36 -0400

    multimapper handling, lineno as tid

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,31 @@ extern crate rust_htslib;
 use std::fs;
 use std::io::Write;
 
-use clap::{Command,Arg};
+use clap::{Command,Arg, ArgAction};
 use rust_htslib::bam;
 use rust_htslib::bam::Read;
 
 // the main logic of the app. perform conversion from SAM/BAM to GTF
-fn convert(input_fname: String, output_fname: String) {
+fn convert(input_fname: String, output_fname: String, keep_multi: bool) {
     let mut reader = bam::Reader::from_path(input_fname).unwrap();
     let header =  reader.header().to_owned();
 
     // prepare the output file
     let mut gtf_writer = fs::File::create(output_fname).expect("Could not create output file");
     
+    let mut read_count: u64 = 0;
     for r in reader.records() {
+        read_count += 1;
+
         let record = r.unwrap();
 
         // if unmapped = skip
         if record.is_unmapped() {
+            continue;
+        }
+
+        // check if multimapper and if keep_multi = false, skip
+        if !keep_multi && record.is_secondary() {
             continue;
         }
 
@@ -83,12 +91,12 @@ fn convert(input_fname: String, output_fname: String) {
         let sequence_id = record.tid();
         let seq_name_bytes = header.tid2name(sequence_id as u32);
         let seq_name = std::str::from_utf8(seq_name_bytes).unwrap();
-        let tx_line = format!("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\ttranscript_id \"{}\";\n", seq_name, "SAM2GTF", "transcript", exon_start, exon_end, ".", strand, ".", qname);
+        let tx_line = format!("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\ttranscript_id \"{}\"; read_name \"{}\";\n", seq_name, "SAM2GTF", "transcript", exons.first().unwrap().0, exons.last().unwrap().1, ".", strand, ".", read_count, qname);
         gtf_writer.write_all(tx_line.as_bytes()).expect("Could not write to output file");
 
         // write exon lines. qname as transcript_id
         for exon in exons {
-            let exon_line = format!("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\ttranscript_id \"{}\";\n", seq_name, "SAM2GTF", "exon", exon.0, exon.1, ".", strand, ".", qname);
+            let exon_line = format!("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\ttranscript_id \"{}\"; read_name \"{}\";\n", seq_name, "SAM2GTF", "exon", exon.0, exon.1, ".", strand, ".", read_count, qname);
             gtf_writer.write_all(exon_line.as_bytes()).expect("Could not write to output file");
         }
 
@@ -111,12 +119,22 @@ fn main() {
             .short('o')
             .long("output")
         )
+        .arg(
+            Arg::new("keep_multi")
+            .short('k')
+            .long("keep_multi")
+            .required(false)
+            .action(ArgAction::SetTrue)
+            .help("If enable, will convert all multimappers. By default this is disabled and only primary alignments are converted."),
+
+        )
         .after_help("--help or -h")
         .get_matches();
 
     // Extract and use the value of the "input" argument
     let input_fname: &String = matches.get_one("input").unwrap();
     let output_fname: &String = matches.get_one("output").unwrap();
+    let keep_multi: bool = matches.get_flag("keep_multi");
 
-    convert(input_fname.to_string(), output_fname.to_string());
+    convert(input_fname.to_string(), output_fname.to_string(), keep_multi);
 }
